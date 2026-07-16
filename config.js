bc09b42b272fdd4d00cc9be9783ea72e3856ba89
tvos96 <tvos96@gmail.com>
2026-07-16 16:01:03 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 // stap-voor-stap uitleg hoe je die aanmaakt (gratis, Google Cloud Console).
 //
 // Voorbeeld: "123456789012-abcdefghijklmnopqrstuvwxyz123456.apps.googleusercontent.com"
-export const GOOGLE_CLIENT_ID = "VUL_HIER_JE_GOOGLE_CLIENT_ID_IN.apps.googleusercontent.com";
+export const GOOGLE_CLIENT_ID = "811734404657-o2v6ca8gkva2ehmlvvu78sf3gvu37u2l.apps.googleusercontent.com";
 
 // Scope: de app kan alleen bestanden lezen/schrijven die ze zelf heeft
 // aangemaakt in jouw Google Drive. Geen toegang tot de rest van je Drive.
